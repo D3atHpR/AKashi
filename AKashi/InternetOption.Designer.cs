9866bd5b8771ea61b367251538db2d4436bb0ea4
D3atHpR <kastiel103@gmail.com>
2020-09-27 21:02:44 -0300

    InternetOptions

--- AKashi/InternetOption.Designer.cs
+++ AKashi/InternetOption.Designer.cs
@@ -138,7 +138,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
-            this.button1.Text = "button1";
+            this.button1.Text = "Use Current";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
@@ -147,7 +147,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
-            this.button2.Text = "button2";
+            this.button2.Text = "Use Blank";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
@@ -171,7 +171,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Cancel";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -180,7 +180,7 @@
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "Ok";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // checkBox1
@@ -188,9 +188,9 @@
             this.checkBox1.AutoSize = true;
             this.checkBox1.Location = new System.Drawing.Point(17, 19);
             this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.Size = new System.Drawing.Size(173, 17);
             this.checkBox1.TabIndex = 0;
-            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.Text = "Delete Browsing History on Exit";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button5
@@ -199,7 +199,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
+            this.button5.Text = "Fonts";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // button6
@@ -208,7 +208,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(75, 23);
             this.button6.TabIndex = 5;
-            this.button6.Text = "button6";
+            this.button6.Text = "Fore Color";
             this.button6.UseVisualStyleBackColor = true;
             // 
             // button7
@@ -217,24 +217,24 @@
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(75, 23);
             this.button7.TabIndex = 6;
-            this.button7.Text = "button7";
+            this.button7.Text = "Back Color";
             this.button7.UseVisualStyleBackColor = true;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(146, 32);
+            this.numericUpDown1.Location = new System.Drawing.Point(201, 32);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(157, 20);
+            this.numericUpDown1.Size = new System.Drawing.Size(102, 20);
             this.numericUpDown1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Location = new System.Drawing.Point(6, 34);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(139, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Number of urls in adrees bar";
             // 
             // InternetOption
             // 
